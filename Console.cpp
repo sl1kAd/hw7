8dff60f6e47108ece291aedcb28c48ac026eb3ad
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2021-05-25 13:52:23 +0300

    Add files via upload

---
+++ Console.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+
+    const int n = 12;
+
+    int* arr = new int[n] { 0, 2, 8, 3, 4, 6, 5, 9, 8, 2, 7, 3 };
+
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = n - 1; j > i; j--) 
+        {
+            if (arr[i] % 2 == 0 && arr[j] % 2 == 0 && arr[i] > arr[j]) 
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+
+    cout << "Вывод:\n";
+
+    for (int i = 0; i < n; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+
+    return 0;
+}
